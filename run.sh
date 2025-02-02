eda09e725be6f4f5968b7231da3af772f8314ffc
Johann Dahm <johann.dahm@gmail.com>
2025-02-02 10:45:55 -0800

    Update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -v $(pwd):/site --workdir /site node:23-alpine3.20 npm install && npm run $1
+docker run -v $(pwd):/site --workdir /site node:23-alpine3.20 /bin/sh -c "npm install && npm run $1"
